5f79bba27f04323d1b6f89fadf5b870f7efb9a88
0smir <o.v.smirnova1987@gmail.com>
2016-04-17 18:08:52 +0300

    add style  for each item of navigation

--- js_dom/dom.js
+++ js_dom/dom.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	var blue = '#00bfff';
 	var	columbiaBlue = '#9bddff';
 	var lightSlateGrey = '#778899';
+	var white = '#FFFFFF';
 
 	
 	//document.getElementById('page-wrapper').style.width = "200px";
@@ -12,25 +13,50 @@ $(document).ready(function(){
 //add nav
 	var nav = document.createElement('ul')
 		nav.className = "main-nav"
-		nav.style.cssText = "width: 95%; min-height: 20px; background-color: #778899; padding: 0; margin: 10px auto;";
+		nav.style.cssText = "width: 95%; min-height: 20px; display: flex; justify-content: space-around; background-color: #778899; padding: 0; margin: 10px auto;";
 
 	document.getElementById('page-wrapper').appendChild(nav);
 
 	var navItem = document.createElement('li')
 		navItem.style.cssText = "display: inline-flex;"
-		navItem.id = "nav-item1";
+		navItem.id = "nav-item1"
+		navItem.innerHTML = "О нас";
 	nav.appendChild(navItem);
 
 	var navItem = document.createElement('li')
 		navItem.style.cssText = "display: inline-flex;"
-		navItem.id = "nav-item2";
+		navItem.id = "nav-item2"
+		navItem.innerHTML = "Услуги";
 	nav.appendChild(navItem);
 
 	var navItem = document.createElement('li')
 		navItem.style.cssText = "display: inline-flex;"
-		navItem.id = "nav-item3";
+		navItem.id = "nav-item3"
+		navItem.innerHTML = "События";
 	nav.appendChild(navItem);
 
+	var navItem = document.createElement('li')
+		navItem.style.cssText = "display: inline-flex;"
+		navItem.id = "nav-item4"
+		navItem.innerHTML = "Каталог";
+	nav.appendChild(navItem);
+
+	//console.log(nav.getElementsByTagName("li"));
+	
+	var navList = document.getElementsByClassName('main-nav')[0];
+		console.log(navList);
+	var list = navList.getElementsByTagName('li');
+	console.log(list);
+	function addColor(data){
+		for(i = 0; i < data.length; i++){
+			data[i].style.color = white;
+		}
+
+	}
+	addColor(list);
+
+	
+	
 
 
 

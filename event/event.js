cefddc8c0738a7e599fbf3d04c4b5464da37093a
0smir <o.v.smirnova1987@gmail.com>
2016-04-20 16:47:39 +0300

    add script for progress bar
    1) add function that implement “growth” of block with red background
    (increase of width);
    2) add event listener for Button;

--- event/event.js
+++ event/event.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+//'use strict'
+ $(document).ready(function(){
 
 
 
@@ -15,6 +16,27 @@ $(document).ready(function(){
 	divLogo.onmouseout = function hideVk(){
 		linkVK.style.cssText = "display: none;";
 	};
+
+
+
+		function move(){
+			var time = setInterval(run, 200);
+			var divLength = 0,
+				element = document.getElementsByClassName('road-inner')[0];
+			function run(){
+				if(divLength == 100){
+					clearInterval(time);
+				}else{
+					divLength++;
+					element.style.width = divLength + "%";
+				}
+			}
+		}
+
+	var divRoad = document.getElementsByClassName('button-aray'),
+		buttonRun = document.getElementById('run');
+	buttonRun.addEventListener("click", move, false);
+	
 
 	
 });

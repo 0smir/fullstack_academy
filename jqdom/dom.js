d4cb6d4acd1ba109c4aab3482836f5fb1a4f9b5e
0smir <o.v.smirnova1987@gmail.com>
2016-04-25 20:26:14 +0300

    add infinity animation

--- jqdom/dom.js
+++ jqdom/dom.js
@@ -9,6 +9,32 @@ $(document).ready(function(){
 		console.log(event.keyCode);
 	});
 
+	$(".send").mouseover(function(event){
+		console.log("pageX:" + event.pageX);
+		console.log("pageY:" + event.pageY);
+
+	});
+
+
+	$(".send").click(function press(){
+		$(".one").animate({
+			left: "250px",
+			opacity: "0.5",
+			height: "150px",
+			width: "150px"
+		}, 4000, function(){
+			$(".one").animate({
+				left: "0",
+				opacity: "0.9",
+				height: "100px",
+				width: "100px"
+
+			}, 3000, press())
+		});
+
+	});
+
+
 
 
 });

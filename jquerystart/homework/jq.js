89503edfb23ce9302425d9ac1e3cf23526787f50
0smir <o.v.smirnova1987@gmail.com>
2016-04-26 15:45:42 +0300

    add script-animation for close button

--- jquerystart/homework/jq.js
+++ jquerystart/homework/jq.js
@@ -9,17 +9,27 @@ $(document).ready(function(){
 
 	$(".wrapp").mouseenter(function(){
 		$(this).find(".title").css({"visibility": "visible"});
-		$(".crossVert").css({'-webkit-transform':'rotate(180deg)'});
+		$(".crossVert").css({'-webkit-transform':'rotate(180deg)',
+								'transition': "transform 0.2s"});
 	});
 
 	$(".wrapp").mouseleave(function(){
 		$(this).find(".title").css({"visibility": "hidden"});
-		$(".crossVert").css({'-webkit-transform':'rotate(90deg)'});
+		$(".crossVert").css({'-webkit-transform':'rotate(90deg)',
+							'-moz-transform':'rotate(90deg)',
+							'-o-transform': 'rotate(90deg)',
+							'transition': "transform 0.3s"});
 	});
 
+
+	$(".close-block").mouseenter(function(){
+		$(".close-block").find(".vert").css({WebkitTransform: "rotate(180deg)",
+												transition: "transform 0.3s"});
+
+	});
+	$(".close-block").mouseleave(function(){
+		$(".close-block").find(".vert").css({WebkitTransform: "rotate(90deg)",
+												transition: "transform 0.3s"});
+
+	});
 });
-
-
-// '-moz-transform':'rotate(90deg)',
-// 												'-webkit-transform':'rotate(90deg)',
-// 												'-o-transform': 'rotate(90deg)'

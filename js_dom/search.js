b5597cddb39d5e4ca64779a8d59c9d667894e81c
0smir <o.v.smirnova1987@gmail.com>
2016-04-26 14:09:09 +0300

    add more items

--- js_dom/search.js
+++ js_dom/search.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+	var red = "#f08080";
+	var pink = "#ffe4e1";
+	var orange = "#ff5a36";
+
 	var address = document.getElementsByClassName('address')[0];
 	var elements = address.getElementsByTagName('*');
 		for(i = 0; i<elements.length; i++){
@@ -18,6 +22,31 @@ $(document).ready(function(){
 	console.log(resultGet[3]);
 	console.log(resultGet[55]);
 	console.log(resultGet.length);
+
+	var citiesList = document.getElementById('cities');
+	var childThird = citiesList.getElementsByTagName('li')[2];
+
+	var newItem = document.createElement('li')
+		newItem.id = 'item5'
+		newItem.innerHTML = "Одесса"
+		newItem.style.backgroundColor = red;
+
+	citiesList.insertBefore(newItem, childThird);
+
+	var newItem = document.createElement('li')
+		newItem.id = 'item5'
+		newItem.innerHTML = "Ужгород"
+		newItem.style.backgroundColor = pink;
+
+	citiesList.insertBefore(newItem, citiesList.firstChild);
+
+	var newItem = document.createElement('li')
+		newItem.id = 'item5'
+		newItem.innerHTML = "Полтава"
+		newItem.style.backgroundColor = orange;
+	citiesList.insertBefore(newItem, citiesList.children[2]);
+
+
 
 
 });

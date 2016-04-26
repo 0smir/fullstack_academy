9270c48bfcc04371e80742c8a1a2e88b7e8e2771
0smir <o.v.smirnova1987@gmail.com>
2016-04-26 14:08:41 +0300

    add script with hall of  mouseleave-mouseenter metod

--- jquerystart/homework/jq.js
+++ jquerystart/homework/jq.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
-	$(".wrapp").hover(function(){
+	// $(".wrapp").hover(function(){
+	// 	$(this).find(".title").css({"visibility": "visible"});
+	// 	$(".close-marker").find(":before").css({"background-color": "red"});
+	// 	}, function(){
+	// 		$(this).find(".title").css({"visibility": "hidden"});
+	// });
+
+
+	$(".wrapp").mouseenter(function(){
 		$(this).find(".title").css({"visibility": "visible"});
-		$(this).css({"background-size": "contain"});
-		}, function(){
-			$(this).find(".title").css({"visibility": "hidden"});
+		$(".crossVert").css({'-webkit-transform':'rotate(180deg)'});
 	});
-	
+
+	$(".wrapp").mouseleave(function(){
+		$(this).find(".title").css({"visibility": "hidden"});
+		$(".crossVert").css({'-webkit-transform':'rotate(90deg)'});
+	});
 
 });
+
+
+// '-moz-transform':'rotate(90deg)',
+// 												'-webkit-transform':'rotate(90deg)',
+// 												'-o-transform': 'rotate(90deg)'

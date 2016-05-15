adc1e0d6842d2e48f6620842e5d7958f7b183510
0smir <o.v.smirnova1987@gmail.com>
2016-05-15 16:42:28 +0300

    add script: count  and adding items of skill-list
    1) add count   count items of list and add result to the HTML;
    2)add function “addItem” - it use:
      - on click to the button #addSkill use method prompt, user write new
    skill ;
      - value of  prompt() method returns and add as inner of HTML;
      - call  function countItems() - for refresh number of skills;

---
+++ javascript_start/additional_tasks/main.js
@@ -0,0 +1,63 @@
+'use strict'
+
+
+	var ul = document.getElementById("list"),
+		itemList = ul.getElementsByTagName('li'),
+		btnAddSkill = document.getElementById('addSkill'),
+		oddElement = ul.querySelectorAll("li:nth-child(odd)"),
+		evenElement = ul.querySelectorAll("li:nth-child(even)");
+
+
+
+	function countItems(data){
+		var listLength = data.length;
+		document.getElementById("skill-count").innerHTML = listLength + " ";
+	}
+	countItems(itemList);
+
+		
+
+	function addColor(list){
+		var list = itemList;
+		itemList = Array.prototype.slice.call(itemList);
+		console.log(itemList);
+
+		itemList.forEach(function(item, index){
+
+			console.log(item);
+			
+			if(index % 2 == 0){
+				item.classList.add('odd');
+			}else{
+				item.classList.add('even');
+			}
+		});
+	}
+	addColor(itemList);
+	
+
+	ul.firstElementChild.classList.add('first');
+
+
+
+	btnAddSkill.addEventListener("click", addItem);
+
+	function addItem(){
+		var newSkill = prompt("Write your new skill!", '');
+		if (newSkill == null){
+			console.log("не заполнено окно")
+		} else if(newSkill == ' '){
+			console.log("не заполнено окно, или пустая строка");	
+		}
+		else{
+			var newItem = document.createElement('li');
+			newItem.innerHTML = newSkill;
+			ul.appendChild(newItem);
+		}
+		
+		countItems(itemList);
+	}
+	
+
+
+

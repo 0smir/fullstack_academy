64a0cd277ce5e4e81c3ef98db34dd6d7c9445381
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-16 22:31:54 +0300

    task#: add script for mobile menu

---
+++ class_project/customize/js/menu/mobile-menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function( $ ) {
+
+    $.mmenu.configuration.classNames.fixedElements = {
+        fixed: "Fixed"
+    };
+
+
+    $("#menu").mmenu({
+        "offCanvas": {
+            "position": "right"
+            // "zposition": "next"
+        },
+        "extensions": [
+            "theme-dark"
+        ],
+        "navbars": [
+            {
+                "position": "bottom",
+                "content": [
+                    "<a class='fa fa-envelope' href='#/'></a>",
+                    "<a class='fa fa-twitter' href='#/'></a>",
+                    "<a class='fa fa-facebook' href='#/'></a>"
+                ]
+            }
+        ]
+    });
+
+    var API = $("#menu").data("mmenu");
+
+    $("#burger").click(function() {
+        API.open();
+    });
+
+});

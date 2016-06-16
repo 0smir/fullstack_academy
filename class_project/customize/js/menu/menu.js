8d11be917409e33987c41b197f93033a2dacd77a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-16 22:29:29 +0300

    task#: add script  that change  background-color of header at scrolling

---
+++ class_project/customize/js/menu/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#header").removeClass("default");
+    $(window).scroll(function(){
+        if($(window).scrollTop() >100){
+            $("#header").addClass("default").fadeIn("fast");
+        } else {
+            $("#header").removeClass("default").fadeIn("fast");
+        }
+    });
+
+    $("#mobile-header").removeClass("default");
+    $(window).scroll(function(){
+        if($(window).scrollTop() >700){
+            $("#mobile-header").addClass("default").fadeIn("fast");
+        } else {
+            $("#mobile-header").removeClass("default").fadeIn("fast");
+        }
+    });
+});

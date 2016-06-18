216cdb328683a4f0986ab439c211db37728f714f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-18 18:08:41 +0300

    Task#: add script for send info from all forms

--- poland-bootstrap/js/sendform.js
+++ poland-bootstrap/js/sendform.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('#myForm').submit(function(e){
+	$('#myForm, #modalForm, myBottomForm').submit(function(e){
 		e.preventDefault();
 		$.ajax({
 			url:'https://formspree.io/o.v.smirnova1987@gmail.com',

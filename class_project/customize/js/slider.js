ae6df6ed1ff651f8d1b41bb1507455ed67449ce1
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-23 01:46:56 +0300

    task#: add script for slider

---
+++ class_project/customize/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-wrapper').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+});

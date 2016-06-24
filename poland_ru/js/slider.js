96851f44a522ca9e7afb6f42bf07b551aebda17b
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-24 18:33:59 +0300

    Task#: add breakpoints for slider, change speed of sliding

--- poland_ru/js/slider.js
+++ poland_ru/js/slider.js
@@ -5,7 +5,43 @@ $(document).ready(function(){
 		infinite: true,
 		slidesToShow: 1,
 		autoplay: true,
-  		autoplaySpeed: 5000,
+  		autoplaySpeed: 8000,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+					autoplay: true,
+					autoplaySpeed: 8000
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+					autoplay: true,
+					autoplaySpeed: 8000
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+					autoplay: true,
+					autoplaySpeed: 8000,
+					arrows: false
+				}
+			}
+		]
 	});
 
 });

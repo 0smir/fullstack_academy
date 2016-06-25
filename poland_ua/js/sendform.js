2fdeee481daf4611b4570d7cd71eb4f15b3984a6
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-25 17:48:21 +0300

    Task#: add changes for script of form submit

--- poland_ua/js/sendform.js
+++ poland_ua/js/sendform.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
 
-	$('#myForm').submit(function(e){
-		e.preventDefault();
+	// $('#myForm').submit(function(e){
+	// 	e.preventDefault();
+	// 	$.ajax({
+	// 		url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+	// 		type:'post',
+	// 		data:$('#myForm').serialize(),
+	// 		success:function(){
+	// 			$('.result-form').find('span').addClass('succsess');
+	// 		}
+	// 	});
+	// });
+
+	$('#myForm').submit(function(event){
+		event.preventDefault();
 		$.ajax({
 			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
 			type:'post',
-			data:$('#myForm').serialize(),
-			success:function(){
-				$('.result-form').find('span').addClass('succsess');	
-			}
+			data: {
+				name: $('[name="name"]').value(),
+				tel: $('[name="phone"]').value(),
+				email: $('[name="mail"]').value(),
+				comment: $('[name="comment"]').value()
+			},
+			dataType: 'json'
+		}).error(function(jqXHR, status, error){
+			alert('Uh oh, something went wrong. Please try again.');
+		}). success(function(data, status, jqXHR) {
+			alert('Thank you for your message. Kitty will get back to you soon.');
+			// $('#myForm').find('[name="name"], #js-message').val('');
 		});
 	});
+
 
 
 });

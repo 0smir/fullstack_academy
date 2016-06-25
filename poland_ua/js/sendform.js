3350edfdca7dd52f7c129fca72de9bba83826dac
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-26 00:14:41 +0300

    del useless code

--- poland_ua/js/sendform.js
+++ poland_ua/js/sendform.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 			dataType: 'json'
 		}).error(function(jqXHR, status, error){
 				$(".error").fadeIn(100).delay(5000).fadeOut(5000);
-			});
 		}).success(function(data, status, jqXHR) {
 				$('#myForm').find('input[name="name"], input[name="phone"], input[name="mail"], textarea[name="comment"]').val('');
 				$(".success").fadeIn(100, function () {

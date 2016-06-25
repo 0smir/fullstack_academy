64c8434a92ab20f7c1cce1d337042aae3fe3986f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-25 21:02:01 +0300

    task#: del useless code in comment; add script for error function

--- poland_ua/js/sendform.js
+++ poland_ua/js/sendform.js
@@ -8,17 +8,13 @@ $(document).ready(function(){
 			data: $('#myForm').serialize(),
 			dataType: 'json'
 		}).error(function(jqXHR, status, error){
-			// $(".results:last-of-type").addClass('error');
-			// $(".results:first-of-type").removeClass('error').delay(5000);
-			$(".error").fadeIn("slow").delay(5000);
+			$(".error").fadeIn("slow", function () {
 			$(".error").fadeOut(3000);
 		}). success(function(data, status, jqXHR) {
 			$('#myForm').find('input[name="name"], input[name="phone"], input[name="mail"], textarea[name="comment"]').val('');
 			$(".success").fadeIn("slow", function () {
 				$(".success").fadeOut(3000);
 			});
-			// $(".results:first-of-type").addClass('success').delay(5000);
-			// $(".results:first-of-type").removeClass('success');
 		});
 	});
 

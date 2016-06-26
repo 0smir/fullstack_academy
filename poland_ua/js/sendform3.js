bcd88f6dabd0f486249084aeaf91328a3064e05a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-26 08:53:02 +0300

    Task#: add block results for all forms; Change script for all forms

--- poland_ua/js/sendform3.js
+++ poland_ua/js/sendform3.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-	$('#myBottomForm').submit(function(e){
-		e.preventDefault();
+
+	$('#myBottomForm').submit(function(event){
+		event.preventDefault();
 		$.ajax({
 			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
 			type:'post',
 			data:$('#myBottomForm').serialize(),
-			success:function(){
-			}
+			dataType: 'json'
+		}).error(function(jqXHR, status, error){
+			$("#poland_11").find(".error").fadeIn(100).delay(5000).fadeOut(5000);
+		}).success(function(data, status, jqXHR) {
+			$('#myBottomForm').find('input[name="name"], input[name="phone"], input[name="mail"], textarea[name="comment"]').val('');
+			$("#poland_11").(".success").fadeIn(100).delay(5000).fadeOut(5000);
 		});
 	});
 
--- poland_ua/js/sendform4.js
+++ poland_ua/js/sendform4.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    $('#callbackRequest').submit(function(e){
-        e.preventDefault();
+    $('#callbackRequest').submit(function(event){
+        event.preventDefault();
         $.ajax({
             url:'https://formspree.io/o.v.smirnova1987@gmail.com',
             type:'post',
             data:$('#callbackRequest').serialize(),
-            success:function(){
-            }
+            dataType: 'json'
+        }).error(function(jqXHR, status, error){
+            $("#modalFormCallback").find(".error").fadeIn(100).delay(5000).fadeOut(5000);
+        }).success(function(data, status, jqXHR) {
+            $('#callbackRequest').find('input[name="name"], input[name="phone"], input[name="mail"], textarea[name="comment"]').val('');
+            $("#modalFormCallback").(".success").fadeIn(100).delay(5000).fadeOut(5000);
         });
     });
 
